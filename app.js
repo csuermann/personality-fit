999c6daa8416f14bf66819c8a44682fbc051ae18
Cornelius Suermann <cornelius.suermann@gmail.com>
2022-01-23 01:05:45 +0100

    improve

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const { Container, Draggable } = VueDndrop;
 
-Pusher.logToConsole = true;
+Pusher.logToConsole = false;
 
 const pusher = new Pusher('8494862854057be56f26', {
   cluster: 'eu',
@@ -8,7 +8,15 @@ const pusher = new Pusher('8494862854057be56f26', {
     'https://0qwey1wi17.execute-api.eu-central-1.amazonaws.com/prod/pusher/auth',
 });
 
-const channel = pusher.subscribe('private-deck-of-cards');
+const match = window.location.href.match(/\?session=(\d{4,7})/);
+
+if (!match) {
+  window.location.href += `?session=${Math.ceil(Math.random() * 1000000)}`;
+} else {
+  sessionId = match[1];
+}
+
+const channel = pusher.subscribe(`private-deck-of-cards-${sessionId}`);
 
 channel.bind('client-page-changed', function ({ pageIndex }) {
   app.activePage = pageIndex;
@@ -19,11 +27,17 @@ channel.bind('client-cards-order-changed', function ({ pageIndex, cards }) {
   app.$forceUpdate();
 });
 
+channel.bind('client-card-selected', function ({ cardId }) {
+  app.selectedCard = cardId;
+});
+
 const app = new Vue({
   el: '#app',
   data: {
     activePage: 0,
+    selectedCard: 'p0',
     pages: [
+      [{ id: 'c0', title: 'connecting...' }],
       [
         { id: 'p1', title: 'Challenging Tasks' },
         { id: 'p2', title: 'Diversity' },
@@ -52,12 +66,19 @@ const app = new Vue({
     numberOfPages: function () {
       return this.pages.length;
     },
+    classObject: function () {
+      return {};
+    },
   },
   components: { Container, Draggable },
   methods: {
+    onDragStart({ payload }) {
+      const { cardId } = payload;
+      channel.trigger('client-card-selected', {
+        cardId,
+      });
+    },
     onDrop(dropResult) {
-      console.log('drop', dropResult);
-
       this.pages[this.activePage] = applyDrag(
         this.pages[this.activePage],
         dropResult
@@ -75,6 +96,13 @@ const app = new Vue({
     changePage(delta) {
       this.activePage += delta;
       channel.trigger('client-page-changed', { pageIndex: this.activePage });
+    },
+    getChildPayload(index) {
+      return {
+        pageIndex: this.activePage,
+        cardIndex: index,
+        cardId: this.pages[this.activePage][index].id,
+      };
     },
   },
 });
--- helpers.js
+++ helpers.js
@@ -1,5 +1,4 @@
 const applyDrag = (arr, dragResult) => {
-  console.log('APPLY-DRAG', arr);
   const { removedIndex, addedIndex, payload } = dragResult;
   if (removedIndex === null && addedIndex === null) {
     return arr;
@@ -14,38 +13,6 @@ const applyDrag = (arr, dragResult) => {
 
   if (addedIndex !== null) {
     result.splice(addedIndex, 0, itemToAdd);
-  }
-
-  console.log('RESULT', result);
-  return result;
-};
-
-// const applyDrag = (arr, dragResult) => {
-//   console.log('APPLY-DRAG', arr);
-//   const { removedIndex, addedIndex, payload } = dragResult;
-//   if (removedIndex === null && addedIndex === null) {
-//     return arr;
-//   }
-
-//   const result = [...arr];
-//   let itemToAdd = payload;
-
-//   if (removedIndex !== null) {
-//     itemToAdd = result.splice(removedIndex, 1)[0];
-//   }
-
-//   if (addedIndex !== null) {
-//     result.splice(addedIndex, 0, itemToAdd);
-//   }
-
-//   console.log('RESULT', result);
-//   return result;
-// };
-
-const generateItems = (count, creator) => {
-  const result = [];
-  for (let i = 0; i < count; i++) {
-    result.push(creator(i));
   }
 
   return result;
